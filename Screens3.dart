b13664de63924f7b0858fc984c4c8516dc1494da
Anshika Srivastava <73658320+Anshika-tech@users.noreply.github.com>
2022-07-02 17:01:13 +0530

    Update Screens3.dart

--- Screens3.dart
+++ Screens3.dart
@@ -17,6 +17,7 @@ class Screens3 extends StatelessWidget {
               SizedBox(
               height: 20,
             ),
+              Image(image:NetworkImage("https://media.istockphoto.com/vectors/certificate-template-banner-with-polygonal-geometric-shape-for-print-vector-id1162482449?k=20&m=1162482449&s=612x612&w=0&h=-BTgHCzj5hVNaqjJ1Rhz3D_yXYPKzNPt-1GRIpf0ieA=") ,width:300),
              Text("You have successfully completed Hybrid Mobile App Development Course",style:TextStyle(fontSize:20)),
 
               SizedBox(
